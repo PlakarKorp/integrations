97080db6e87c3e8b9b8e1e5883b6e4ba50356a48
Peeyush Aggarwal <peeyushaggarwal94@gmail.com>
2026-09-25 22:32:46 +0530

    routeros: use testify in location error test

--- routeros/connector_test.go
+++ routeros/connector_test.go
@@ -2,18 +2,16 @@ package routeros
 
 import (
 	"context"
-	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNewRedactsCredentials(t *testing.T) {
 	loc := "routeros+export://user:secret@host/%zz"
 	_, err := NewImporter(context.Background(), nil, "routeros+export",
 		map[string]string{"location": loc})
-	if err == nil {
-		t.Fatalf("NewImporter(%q): expected error", loc)
-	}
-	if strings.Contains(err.Error(), "secret") {
-		t.Errorf("NewImporter(%q): error leaks password: %v", loc, err)
-	}
+	require.Error(t, err)
+	assert.NotContains(t, err.Error(), "secret")
 }
